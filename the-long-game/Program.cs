111a94f6550e8fa015f2fa793fe1715510007d23
Gavin <111817327+gavinwebst@users.noreply.github.com>
2024-11-07 09:15:06 -0500

    homework

---
+++ the-long-game/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace the_long_game
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, what is your name?");
+            string user = Console.ReadLine();
+
+            //creating a text file path to store the user score;
+            string filepath = user + ".txt";
+
+            //starting score
+            int score = 0;
+            
+          
+
+
+
+        }
+
+    }
+}
